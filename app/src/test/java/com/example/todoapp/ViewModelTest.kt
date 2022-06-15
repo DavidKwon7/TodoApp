96a9bbdcf0afe05bdbeeadb39b842ca1c2b57de7
DavidKwon7 <khjunejune10@gmail.com>
2022-06-15 19:51:51 +0900

    appTestModule 작성

--- app/src/test/java/com/example/todoapp/ViewModelTest.kt
+++ app/src/test/java/com/example/todoapp/ViewModelTest.kt
@@ -1,7 +1,14 @@
 package com.example.todoapp
 
+import android.app.Application
+import com.example.todoapp.di.appTestModule
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Before
 import org.junit.Rule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import org.koin.test.KoinTest
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
 
@@ -9,4 +16,17 @@ internal class ViewModelTest : KoinTest {
 
     @get:Rule
     val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    private lateinit var context: Application
+
+    private val dispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setup() {
+        startKoin {
+            androidContext(context)
+            modules(appTestModule)
+        }
+    }
 }
---
+++ app/src/test/java/com/example/todoapp/di/appTestModule.kt
@@ -0,0 +1,5 @@
+package com.example.todoapp.di
+
+import org.koin.dsl.module
+
+internal val appTestModule = module  { }
