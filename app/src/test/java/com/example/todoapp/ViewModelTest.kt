3034c2851eb548494699f176686de4ce05009e97
DavidKwon7 <khjunejune10@gmail.com>
2022-06-15 19:45:25 +0900

    mockitoRule 작성

--- app/src/test/java/com/example/todoapp/ViewModelTest.kt
+++ app/src/test/java/com/example/todoapp/ViewModelTest.kt
@@ -2,7 +2,11 @@ package com.example.todoapp
 
 import org.junit.Rule
 import org.koin.test.KoinTest
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
 
-class ViewModelTest : KoinTest {
+internal class ViewModelTest : KoinTest {
 
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
 }
