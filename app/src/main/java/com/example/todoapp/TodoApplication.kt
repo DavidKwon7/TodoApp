d655e823baa7fb66d328e7a129e2353d27389b97
DavidKwon7 <khjunejune10@gmail.com>
2022-06-15 16:15:35 +0900

    TodoApplication : koin trigger 작성

--- app/src/main/java/com/example/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/todoapp/TodoApplication.kt
@@ -1,10 +1,19 @@
 package com.example.todoapp
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class TodoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        //todo koin trigger
+        // koin trigger
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@TodoApplication)
+            modules()
+        }
     }
 }
