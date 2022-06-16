5a6bfcaa0310a81b541f940ca75a9e0c9c84bc4b
DavidKwon7 <khjunejune10@gmail.com>
2022-06-16 14:44:06 +0900

    TodoRepository 작성

--- app/src/main/java/com/example/todoapp/data/local/repository/TodoRepository.kt
+++ app/src/main/java/com/example/todoapp/data/local/repository/TodoRepository.kt
@@ -1,4 +1,17 @@
 package com.example.todoapp.data.local.repository
 
+import com.example.todoapp.data.entity.ToDoEntity
+
 interface TodoRepository {
+
+    suspend fun getToDoLIst(): List<ToDoEntity>
+    suspend fun getToDoItem(id:Long): ToDoEntity?
+
+    suspend fun insertToDOItem(toDoEntity: ToDoEntity): Long
+    suspend fun insertToDoList(toDoList: List<ToDoEntity>)
+
+    suspend fun updateToDoItem(toDoEntity: ToDoEntity)
+
+    suspend fun deleteToDoItem(id: Long)
+    suspend fun deleteAll()
 }
