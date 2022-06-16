8e17ed0101e6e0a5f2f3f4d420b98e4423ba203e
DavidKwon7 <khjunejune10@gmail.com>
2022-06-17 00:52:00 +0900

    RepositoryImpl 작성

--- app/src/main/java/com/example/todoapp/data/local/repository/DefaultToDoRepository.kt
+++ app/src/main/java/com/example/todoapp/data/local/repository/DefaultToDoRepository.kt
@@ -3,37 +3,38 @@ package com.example.todoapp.data.local.repository
 import com.example.todoapp.data.entity.ToDoEntity
 import com.example.todoapp.data.local.db.dao.ToDoDao
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 class DefaultToDoRepository(
     private val toDoDao: ToDoDao,
     private val ioDispatcher: CoroutineDispatcher
 ): TodoRepository {
 
-    override suspend fun getToDoLIst(): List<ToDoEntity> {
-        TODO("Not yet implemented")
+    override suspend fun getToDoLIst(): List<ToDoEntity> = withContext(ioDispatcher) {
+        toDoDao.getAll()
     }
 
-    override suspend fun getToDoItem(id: Long): ToDoEntity? {
-        TODO("Not yet implemented")
+    override suspend fun getToDoItem(id: Long): ToDoEntity? = withContext(ioDispatcher) {
+        toDoDao.getById(id)
     }
 
-    override suspend fun insertToDOItem(toDoEntity: ToDoEntity): Long {
-        TODO("Not yet implemented")
+    override suspend fun insertToDOItem(toDoEntity: ToDoEntity): Long = withContext(ioDispatcher) {
+        toDoDao.insert(toDoEntity = toDoEntity)
     }
 
-    override suspend fun insertToDoList(toDoList: List<ToDoEntity>) {
-        TODO("Not yet implemented")
+    override suspend fun insertToDoList(toDoList: List<ToDoEntity>) = withContext(ioDispatcher) {
+        toDoDao.insert(toDoList)
     }
 
-    override suspend fun updateToDoItem(toDoEntity: ToDoEntity) {
-        TODO("Not yet implemented")
+    override suspend fun updateToDoItem(toDoEntity: ToDoEntity) = withContext(ioDispatcher) {
+        toDoDao.update(toDoEntity)
     }
 
-    override suspend fun deleteToDoItem(id: Long) {
-        TODO("Not yet implemented")
+    override suspend fun deleteToDoItem(id: Long) = withContext(ioDispatcher) {
+        toDoDao.delete(id)
     }
 
-    override suspend fun deleteAll() {
-        TODO("Not yet implemented")
+    override suspend fun deleteAll() = withContext(ioDispatcher) {
+        toDoDao.deleteAll()
     }
 }
