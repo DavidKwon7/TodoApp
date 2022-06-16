5aa2d6e44b17c4ecc9f914c29320d46a35b893e2
DavidKwon7 <khjunejune10@gmail.com>
2022-06-16 14:55:33 +0900

    TodoRepository 테스트 모듈 추가

--- app/src/test/java/com/example/todoapp/di/appTestModule.kt
+++ app/src/test/java/com/example/todoapp/di/appTestModule.kt
@@ -1,5 +1,11 @@
 package com.example.todoapp.di
 
+import com.example.todoapp.data.local.repository.TodoRepository
+import com.example.todoapp.data.repository.TestToDoRepository
 import org.koin.dsl.module
 
-internal val appTestModule = module { }
+internal val appTestModule = module {
+
+
+    single<TodoRepository> { TestToDoRepository() }
+}
