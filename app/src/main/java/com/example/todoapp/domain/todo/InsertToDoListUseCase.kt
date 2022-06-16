f20e665fc7212047440280403c0b2ffdb50119ec
DavidKwon7 <khjunejune10@gmail.com>
2022-06-16 15:09:19 +0900

    insertUseCases 작성

---
+++ app/src/main/java/com/example/todoapp/domain/todo/InsertToDoListUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp.domain.todo
+
+import com.example.todoapp.data.entity.ToDoEntity
+import com.example.todoapp.data.local.repository.TodoRepository
+import com.example.todoapp.domain.UseCase
+
+class InsertToDoListUseCase (
+    private val todoRepository: TodoRepository
+    ) : UseCase {
+        suspend operator fun invoke(toDoEntity: List<ToDoEntity>) {
+            return todoRepository.insertToDoList(toDoEntity)
+        }
+
+}
