af34bf14bf4109aa24f35e9372e233a57ba2b70a
DavidKwon7 <khjunejune10@gmail.com>
2022-06-17 16:53:20 +0900

    MainActivity -> ListActivity 이름 변경

--- app/src/main/java/com/example/todoapp/presentation/list/ListActivity.kt
+++ app/src/main/java/com/example/todoapp/presentation/list/ListActivity.kt
@@ -1,9 +1,10 @@
-package com.example.todoapp
+package com.example.todoapp.presentation.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
