349c9f6a1b34ca341444c58cfbe057e6e7292f53
DavidKwon7 <khjunejune10@gmail.com>
2022-06-17 15:15:31 +0900

    DetailViewModel.kt 작성 🔥

--- app/src/main/java/com/example/todoapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/detail/DetailViewModel.kt
@@ -4,12 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todoapp.data.entity.ToDoEntity
 import com.example.todoapp.domain.todo.DeleteToDoItemUseCase
 import com.example.todoapp.domain.todo.GetToDoItemUseCase
 import com.example.todoapp.domain.todo.InsertToDoUseCase
 import com.example.todoapp.domain.todo.UpdateToDoUseCase
 import com.example.todoapp.presentation.list.DetailMode
 import kotlinx.coroutines.launch
+import java.lang.Exception
+
+/** vm은 최종 도착 단계가 아닌, 거쳐가는 중간 단계.
+ * 그렇기에 상태관리 및 liveData 등을 사용한 데이터 전달 역할을 수행
+ */
+
 
 internal class DetailViewModel(
     var detailMode: DetailMode,
@@ -26,10 +33,65 @@ internal class DetailViewModel(
     fun fetchData() = viewModelScope.launch {
         when(detailMode) {
             DetailMode.WRITE -> {
-
+                _toDoDetailLiveData.postValue(ToDoDetailState.Write)
             }
             DetailMode.DETAIL -> {
+                _toDoDetailLiveData.postValue(ToDoDetailState.Loading)
+                try {
+                    getToDoItemUseCase(id)?.let {
+                        _toDoDetailLiveData.postValue(ToDoDetailState.Success(it))
+                    } ?: kotlin.run {
+                        _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+                    }
+                } catch (e:Exception) {
+                    e.printStackTrace()
+                    _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+                }
+            }
+        }
+    }
 
+    fun deleteToDo() = viewModelScope.launch {
+        _toDoDetailLiveData.postValue(ToDoDetailState.Loading)  // 기본 상태는 로딩!
+        try {
+            deleteToDoItemUseCase(id)
+            _toDoDetailLiveData.postValue(ToDoDetailState.Delete)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+        }
+    }
+
+    fun setModifyMode() = viewModelScope.launch {
+        _toDoDetailLiveData.postValue(ToDoDetailState.Modify)
+    }
+
+    // todo Study it!
+    fun writeToDo(title:String, description: String) = viewModelScope.launch {
+        _toDoDetailLiveData.postValue(ToDoDetailState.Loading)
+        when(detailMode) {
+            DetailMode.WRITE -> {
+                try {
+                    val toDoEntity = ToDoEntity(title = title, description = description)
+                    id = insertToDoUseCase(toDoEntity)
+                    _toDoDetailLiveData.postValue(ToDoDetailState.Success(toDoEntity))
+                    detailMode = DetailMode.DETAIL
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+                }
+            }
+            DetailMode.DETAIL -> {
+                try {
+                    getToDoItemUseCase(id)?.let {
+                        val updateToDoEntity = it.copy(title = title, description = description)
+                        updateToDoUseCase(updateToDoEntity)
+                        _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    _toDoDetailLiveData.postValue(ToDoDetailState.Error)
+                }
             }
         }
     }
