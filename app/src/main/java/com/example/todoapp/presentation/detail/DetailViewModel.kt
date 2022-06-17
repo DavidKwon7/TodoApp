31e331790eb1618a8a62d7ff8df7abd6918a66b3
DavidKwon7 <khjunejune10@gmail.com>
2022-06-17 14:57:07 +0900

    DetailViewModel.kt fetchData 기본형 완성

--- app/src/main/java/com/example/todoapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/detail/DetailViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.todoapp.presentation.detail
 
-class DetailViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.domain.todo.DeleteToDoItemUseCase
+import com.example.todoapp.domain.todo.GetToDoItemUseCase
+import com.example.todoapp.domain.todo.InsertToDoUseCase
+import com.example.todoapp.domain.todo.UpdateToDoUseCase
+import com.example.todoapp.presentation.list.DetailMode
+import kotlinx.coroutines.launch
+
+internal class DetailViewModel(
+    var detailMode: DetailMode,
+    var id: Long = -1,
+    private val getToDoItemUseCase: GetToDoItemUseCase,
+    private val deleteToDoItemUseCase: DeleteToDoItemUseCase,
+    private val updateToDoUseCase: UpdateToDoUseCase,
+    private val insertToDoUseCase: InsertToDoUseCase
+) : ViewModel() {
+
+    private var _toDoDetailLiveData = MutableLiveData<ToDoDetailState>(ToDoDetailState.UnInitialized)
+    val toDoDetailLiveData: LiveData<ToDoDetailState> = _toDoDetailLiveData
+
+    fun fetchData() = viewModelScope.launch {
+        when(detailMode) {
+            DetailMode.WRITE -> {
+
+            }
+            DetailMode.DETAIL -> {
+
+            }
+        }
+    }
 }
