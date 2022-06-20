b7835e1ad1d612203b472e34a706673271b7c9cf
DavidKwon7 <khjunejune10@gmail.com>
2022-06-20 15:55:17 +0900

    DetailActivity.kt : initViews fun 작성

--- app/src/main/java/com/example/todoapp/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/example/todoapp/presentation/detail/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todoapp.presentation.detail
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.core.view.isGone
 import com.example.todoapp.databinding.ActivityDetailBinding
 import com.example.todoapp.presentation.base.BaseActivity
 import com.example.todoapp.presentation.list.DetailMode
@@ -16,6 +19,8 @@ internal class DetailActivity : BaseActivity<DetailViewModel>() {
     override val viewModel: DetailViewModel by viewModel {
         parametersOf(
             // 받아올 데이터 intent로 가져오기
+            intent.getSerializableExtra(DETAIL_MODE_KEY),
+            intent.getLongExtra(TODO_ID_KEY, -1)
         )
     }
 
@@ -38,11 +43,43 @@ internal class DetailActivity : BaseActivity<DetailViewModel>() {
             putExtra(TODO_ID_KEY, id)
             putExtra(DETAIL_MODE_KEY, detailMode)
         }
-
     }
 
-    override fun observeData() {
-        TODO("Not yet implemented")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setResult(Activity.RESULT_OK)
+    }
+
+    override fun observeData() = viewModel.toDoDetailLiveData.observe(this) {
+        when(it) {
+            is ToDoDetailState.UnInitialized -> {
+                initViews(binding)
+            }
+        }
+    }
+
+    private fun initViews(binding: ActivityDetailBinding) = with(binding) {
+        titleInput.isEnabled = false
+        descriptionInput.isEnabled = false
+
+        deleteButton.isGone = true
+        modifyButton.isGone = true
+        updateButton.isGone = true
+
+        deleteButton.setOnClickListener {
+            viewModel.deleteToDo()
+        }
+        modifyButton.setOnClickListener {
+            viewModel.setModifyMode()
+        }
+        updateButton.setOnClickListener {
+            viewModel.writeToDo(
+                title = titleInput.text.toString(),
+                description = descriptionInput.text.toString()
+            )
+        }
     }
 
 }
